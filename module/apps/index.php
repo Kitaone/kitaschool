10c5a2f7b611f7aee736065e1c4978997b2de013
gilangsadewo71 <gilangsadewo71@gmail.com>
2022-08-27 13:52:32 +0700

    up tab apps

--- module/apps/index.php
+++ module/apps/index.php
@@ -1,56 +1,282 @@
 <?php 
 $aksi   = "module/".$_GET['module']."/action.php";
 ?>
-<div class="col-md-12">
-  <div class="card">
-    <div class="card-header card-header-primary">
-      <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create"><i class="fa fa-plus"></i></a></span>
-      <h4 class="card-title "><?php echo strtolower($_GET['module']) ?></h4>
-      <p class="card-category"> Tabel dengan data <?php echo strtolower($_GET['module']) ?></p>
-    </div>
-    <div class="card-body">
-      <div class="p-2">
-        <?php 
-        if (isset($_SESSION['flash'])): ?>
-          <div class="<?php echo $_SESSION['flash']['class']; ?> mt-3 mb-3"> 
-            <i class="<?php echo $_SESSION['flash']['icon'] ?>"></i> <?php echo $_SESSION['flash']['label']; ?>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+
+<nav>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <a class="nav-item nav-link active" id="nav-apps-tab" data-toggle="tab" href="#apps" role="tab" aria-controls="nav-apps" aria-selected="true">Apps</a>
+    <a class="nav-item nav-link" id="nav-appsuser-tab" data-toggle="tab" href="#appsuser" role="tab" aria-controls="nav-appsuser" aria-selected="false">Apps User</a>
+    <a class="nav-item nav-link" id="nav-appsmenu-tab" data-toggle="tab" href="#appsmenu" role="tab" aria-controls="nav-appsmenu" aria-selected="false">Apps Menu</a>
+    <a class="nav-item nav-link" id="nav-appsmenuuser-tab" data-toggle="tab" href="#appsmenuuser" role="tab" aria-controls="nav-appsmenuuser" aria-selected="false">Apps Menu User</a>
+    <a class="nav-item nav-link" id="nav-appspermission-tab" data-toggle="tab" href="#appspermission" role="tab" aria-controls="nav-appspermission" aria-selected="false">Apps Permission</a>
+  </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+  <div class="tab-pane fade show active" id="apps" role="tabpanel" aria-labelledby="nav-apps-tab">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header card-header-primary">
+          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create"><i class="fa fa-plus"></i></a></span>
+          <h4 class="card-title "><?php echo strtolower($_GET['module']) ?></h4>
+          <p class="card-category"> Tabel dengan data <?php echo strtolower($_GET['module']) ?></p>
+        </div>
+        <div class="card-body">
+          <div class="p-2">
+            <?php 
+            if (isset($_SESSION['flash'])): ?>
+              <div class="<?php echo $_SESSION['flash']['class']; ?> mt-3 mb-3"> 
+                <i class="<?php echo $_SESSION['flash']['icon'] ?>"></i> <?php echo $_SESSION['flash']['label']; ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+              </div>
+            <?php endif ?>
           </div>
-        <?php endif ?>
+          <div class="table-responsive">
+            <table class="table">
+              <thead class=" text-primary">
+                <th>#</th>           
+                <th>Hcode</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Status</th>
+                <th>Created at</th>   
+                <th>Updated at</th>   
+                <th>Aksi</th>
+              </thead>
+              <tbody>            
+                <tr>       
+                  <td>1</td>
+                  <td>dashboard</td>
+                  <td>Dashboard</td>
+                  <td>Lorem ipsum dolor siamet</td>
+                  <td>1</td>
+                  <td>2022-08-20 10:46:58</td>
+                  <td>2022-08-20 10:46:58</td>
+                  <td>
+                    <span>
+                      <a class="btn btn-primary btn-xs"><i style="color:white;" class="fa fa-edit"></i></a>
+                      <a class="btn btn-danger btn-xs"><i style="color:white;" class="fa fa-trash"></i></a>
+                    </span>
+                  </td>
+                </tr>              
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-      <div class="table-responsive">
-        <table class="table">
-          <thead class=" text-primary">
-            <th>#</th>
-            <th>School Token</th>
-            <th>Hcode</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Status</th>
-            <th>Created at</th>   
-            <th>Updated at</th>   
-            <th>Aksi</th>
-          </thead>
-          <tbody>            
-            <tr>       
-              <td>1</td>
-              <td>aF5I7jy9nZmGEFY16&zhlIebtI^</td>
-              <td>dashboard</td>
-              <td>Dashboard</td>
-              <td>Lorem ipsum dolor siamet</td>
-              <td>1</td>
-              <td>2022-08-20 10:46:58</td>
-              <td>2022-08-20 10:46:58</td>
-              <td>
-                <span>
-                  <a class="btn btn-primary btn-xs"><i style="color:white;" class="fa fa-edit"></i></a>
-                  <a class="btn btn-danger btn-xs"><i style="color:white;" class="fa fa-trash"></i></a>
-                </span>
-              </td>
-            </tr>              
-          </tbody>
-        </table>
+    </div>
+  </div>
+  <div class="tab-pane fade" id="appsuser" role="tabpanel" aria-labelledby="nav-appsuser-tab">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header card-header-primary">
+          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create"><i class="fa fa-plus"></i></a></span>
+          <h4 class="card-title "><?php echo strtolower($_GET['module']) ?> user</h4>
+          <p class="card-category"> Tabel dengan data <?php echo strtolower($_GET['module']) ?> user</p>
+        </div>
+        <div class="card-body">
+          <div class="p-2">
+            <?php 
+            if (isset($_SESSION['flash'])): ?>
+              <div class="<?php echo $_SESSION['flash']['class']; ?> mt-3 mb-3"> 
+                <i class="<?php echo $_SESSION['flash']['icon'] ?>"></i> <?php echo $_SESSION['flash']['label']; ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+              </div>
+            <?php endif ?>
+          </div>
+          <div class="table-responsive">
+            <table class="table">
+              <thead class=" text-primary">
+                <th>#</th>           
+                <th>Hcode</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Status</th>
+                <th>Created at</th>   
+                <th>Updated at</th>   
+                <th>Aksi</th>
+              </thead>
+              <tbody>            
+                <tr>       
+                  <td>1</td>
+                  <td>dashboard</td>
+                  <td>Dashboard</td>
+                  <td>Lorem ipsum dolor siamet</td>
+                  <td>1</td>
+                  <td>2022-08-20 10:46:58</td>
+                  <td>2022-08-20 10:46:58</td>
+                  <td>
+                    <span>
+                      <a class="btn btn-primary btn-xs"><i style="color:white;" class="fa fa-edit"></i></a>
+                      <a class="btn btn-danger btn-xs"><i style="color:white;" class="fa fa-trash"></i></a>
+                    </span>
+                  </td>
+                </tr>              
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="tab-pane fade" id="appsmenu" role="tabpanel" aria-labelledby="nav-appsmenu-tab">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header card-header-primary">
+          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create"><i class="fa fa-plus"></i></a></span>
+          <h4 class="card-title "><?php echo strtolower($_GET['module']) ?> menu</h4>
+          <p class="card-category"> Tabel dengan data <?php echo strtolower($_GET['module']) ?> menu</p>
+        </div>
+        <div class="card-body">
+          <div class="p-2">
+            <?php 
+            if (isset($_SESSION['flash'])): ?>
+              <div class="<?php echo $_SESSION['flash']['class']; ?> mt-3 mb-3"> 
+                <i class="<?php echo $_SESSION['flash']['icon'] ?>"></i> <?php echo $_SESSION['flash']['label']; ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+              </div>
+            <?php endif ?>
+          </div>
+          <div class="table-responsive">
+            <table class="table">
+              <thead class=" text-primary">
+                <th>#</th>           
+                <th>Hcode</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Status</th>
+                <th>Created at</th>   
+                <th>Updated at</th>   
+                <th>Aksi</th>
+              </thead>
+              <tbody>            
+                <tr>       
+                  <td>1</td>
+                  <td>dashboard</td>
+                  <td>Dashboard</td>
+                  <td>Lorem ipsum dolor siamet</td>
+                  <td>1</td>
+                  <td>2022-08-20 10:46:58</td>
+                  <td>2022-08-20 10:46:58</td>
+                  <td>
+                    <span>
+                      <a class="btn btn-primary btn-xs"><i style="color:white;" class="fa fa-edit"></i></a>
+                      <a class="btn btn-danger btn-xs"><i style="color:white;" class="fa fa-trash"></i></a>
+                    </span>
+                  </td>
+                </tr>              
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="tab-pane fade" id="appsmenuuser" role="tabpanel" aria-labelledby="nav-appsmenuuser-tab">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header card-header-primary">
+          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create"><i class="fa fa-plus"></i></a></span>
+          <h4 class="card-title "><?php echo strtolower($_GET['module']) ?> menu user</h4>
+          <p class="card-category"> Tabel dengan data <?php echo strtolower($_GET['module']) ?> menu user</p>
+        </div>
+        <div class="card-body">
+          <div class="p-2">
+            <?php 
+            if (isset($_SESSION['flash'])): ?>
+              <div class="<?php echo $_SESSION['flash']['class']; ?> mt-3 mb-3"> 
+                <i class="<?php echo $_SESSION['flash']['icon'] ?>"></i> <?php echo $_SESSION['flash']['label']; ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+              </div>
+            <?php endif ?>
+          </div>
+          <div class="table-responsive">
+            <table class="table">
+              <thead class=" text-primary">
+                <th>#</th>           
+                <th>Hcode</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Status</th>
+                <th>Created at</th>   
+                <th>Updated at</th>   
+                <th>Aksi</th>
+              </thead>
+              <tbody>            
+                <tr>       
+                  <td>1</td>
+                  <td>dashboard</td>
+                  <td>Dashboard</td>
+                  <td>Lorem ipsum dolor siamet</td>
+                  <td>1</td>
+                  <td>2022-08-20 10:46:58</td>
+                  <td>2022-08-20 10:46:58</td>
+                  <td>
+                    <span>
+                      <a class="btn btn-primary btn-xs"><i style="color:white;" class="fa fa-edit"></i></a>
+                      <a class="btn btn-danger btn-xs"><i style="color:white;" class="fa fa-trash"></i></a>
+                    </span>
+                  </td>
+                </tr>              
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="tab-pane fade" id="appspermission" role="tabpanel" aria-labelledby="nav-appspermission-tab">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header card-header-primary">
+          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create"><i class="fa fa-plus"></i></a></span>
+          <h4 class="card-title "><?php echo strtolower($_GET['module']) ?> menu permission</h4>
+          <p class="card-category"> Tabel dengan data <?php echo strtolower($_GET['module']) ?> menu permission</p>
+        </div>
+        <div class="card-body">
+          <div class="p-2">
+            <?php 
+            if (isset($_SESSION['flash'])): ?>
+              <div class="<?php echo $_SESSION['flash']['class']; ?> mt-3 mb-3"> 
+                <i class="<?php echo $_SESSION['flash']['icon'] ?>"></i> <?php echo $_SESSION['flash']['label']; ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+              </div>
+            <?php endif ?>
+          </div>
+          <div class="table-responsive">
+            <table class="table">
+              <thead class=" text-primary">
+                <th>#</th>           
+                <th>Hcode</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Status</th>
+                <th>Created at</th>   
+                <th>Updated at</th>   
+                <th>Aksi</th>
+              </thead>
+              <tbody>            
+                <tr>       
+                  <td>1</td>
+                  <td>dashboard</td>
+                  <td>Dashboard</td>
+                  <td>Lorem ipsum dolor siamet</td>
+                  <td>1</td>
+                  <td>2022-08-20 10:46:58</td>
+                  <td>2022-08-20 10:46:58</td>
+                  <td>
+                    <span>
+                      <a class="btn btn-primary btn-xs"><i style="color:white;" class="fa fa-edit"></i></a>
+                      <a class="btn btn-danger btn-xs"><i style="color:white;" class="fa fa-trash"></i></a>
+                    </span>
+                  </td>
+                </tr>              
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
