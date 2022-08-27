ba22129e0c357893dadb76de01e27ac23c153f3c
gilangsadewo71 <gilangsadewo71@gmail.com>
2022-08-27 14:56:48 +0700

    add module create apps user, apps menu, apps menu user, apps permission

---
+++ module/apps/create_apps_permission.php
@@ -0,0 +1,45 @@
+<?php 
+$aksi   = "module/".$_GET['module']."/action.php";
+$edit=mysqli_fetch_array(mysqli_query($conn,"SELECT * from pasien where id='$_GET[id]'"));
+?>
+<div class="col-md-12">
+  <div class="card">
+    <div class="card-header card-header-primary">
+      
+      <h4 class="card-title"><?php echo strtolower($_GET['act']) ?></h4>
+      <p class="card-category">Pengisian yang belum disimpan akan hilang</p>
+    </div>
+    <div class="card-body">
+      <form action="<?php echo $aksi."?module=".$_GET['module']."&act=".$_GET['act'] ?>" method="POST">        
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <label class="bmd-label-floating">Role id</label>
+              <input type="text" class="form-control">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <label class="bmd-label-floating">Permission id</label>
+              <input type="text" class="form-control">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <label class="bmd-label-floating">Status</label>
+              <select class="form-control custom-select">
+                <option selected> - Choose Status - </option>
+                <option value="1">Active</option>
+                <option value="0">Non Active</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
--- module/apps/index.php
+++ module/apps/index.php
@@ -11,7 +11,7 @@ $aksi   = "module/".$_GET['module']."/action.php";
     <a class="nav-item nav-link" id="nav-appspermission-tab" data-toggle="tab" href="#appspermission" role="tab" aria-controls="nav-appspermission" aria-selected="false">Apps Permission</a>
   </div>
 </nav>
-<div class="tab-content" id="nav-tabContent">
+<div class="tab-content col-md-12" id="nav-tabContent">
   <div class="tab-pane fade show active" id="apps" role="tabpanel" aria-labelledby="nav-apps-tab">
     <div class="col-md-12">
       <div class="card">
@@ -69,7 +69,7 @@ $aksi   = "module/".$_GET['module']."/action.php";
     <div class="col-md-12">
       <div class="card">
         <div class="card-header card-header-primary">
-          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create"><i class="fa fa-plus"></i></a></span>
+          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create_apps_user"><i class="fa fa-plus"></i></a></span>
           <h4 class="card-title "><?php echo strtolower($_GET['module']) ?> user</h4>
           <p class="card-category"> Tabel dengan data <?php echo strtolower($_GET['module']) ?> user</p>
         </div>
@@ -87,9 +87,9 @@ $aksi   = "module/".$_GET['module']."/action.php";
             <table class="table">
               <thead class=" text-primary">
                 <th>#</th>           
-                <th>Hcode</th>
-                <th>Name</th>
-                <th>Description</th>
+                <th>Role id</th>
+                <th>User id</th>
+                <th>Apps id</th>
                 <th>Status</th>
                 <th>Created at</th>   
                 <th>Updated at</th>   
@@ -98,9 +98,9 @@ $aksi   = "module/".$_GET['module']."/action.php";
               <tbody>            
                 <tr>       
                   <td>1</td>
-                  <td>dashboard</td>
-                  <td>Dashboard</td>
-                  <td>Lorem ipsum dolor siamet</td>
+                  <td>6</td>
+                  <td>1</td>
+                  <td>2</td>
                   <td>1</td>
                   <td>2022-08-20 10:46:58</td>
                   <td>2022-08-20 10:46:58</td>
@@ -122,7 +122,7 @@ $aksi   = "module/".$_GET['module']."/action.php";
     <div class="col-md-12">
       <div class="card">
         <div class="card-header card-header-primary">
-          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create"><i class="fa fa-plus"></i></a></span>
+          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create_apps_menu"><i class="fa fa-plus"></i></a></span>
           <h4 class="card-title "><?php echo strtolower($_GET['module']) ?> menu</h4>
           <p class="card-category"> Tabel dengan data <?php echo strtolower($_GET['module']) ?> menu</p>
         </div>
@@ -140,9 +140,15 @@ $aksi   = "module/".$_GET['module']."/action.php";
             <table class="table">
               <thead class=" text-primary">
                 <th>#</th>           
-                <th>Hcode</th>
-                <th>Name</th>
-                <th>Description</th>
+                <th>Parent id</th>
+                <th>Menu Name</th>
+                <th>Slug</th>
+                <th>Menu Description</th>
+                <th>Menu URL</th>
+                <th>Menu Order</th>
+                <th>Menu Type</th>
+                <th>Menu Icon</th>
+                <th>Root</th>
                 <th>Status</th>
                 <th>Created at</th>   
                 <th>Updated at</th>   
@@ -151,9 +157,15 @@ $aksi   = "module/".$_GET['module']."/action.php";
               <tbody>            
                 <tr>       
                   <td>1</td>
-                  <td>dashboard</td>
+                  <td>2</td>
                   <td>Dashboard</td>
-                  <td>Lorem ipsum dolor siamet</td>
+                  <td>home</td>
+                  <td>Master Dashboard</td>
+                  <td>dashboard</td>
+                  <td>2</td>
+                  <td>menu</td>
+                  <td><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></td>
+                  <td>1</td>
                   <td>1</td>
                   <td>2022-08-20 10:46:58</td>
                   <td>2022-08-20 10:46:58</td>
@@ -175,7 +187,7 @@ $aksi   = "module/".$_GET['module']."/action.php";
     <div class="col-md-12">
       <div class="card">
         <div class="card-header card-header-primary">
-          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create"><i class="fa fa-plus"></i></a></span>
+          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create_apps_menu_user"><i class="fa fa-plus"></i></a></span>
           <h4 class="card-title "><?php echo strtolower($_GET['module']) ?> menu user</h4>
           <p class="card-category"> Tabel dengan data <?php echo strtolower($_GET['module']) ?> menu user</p>
         </div>
@@ -193,20 +205,20 @@ $aksi   = "module/".$_GET['module']."/action.php";
             <table class="table">
               <thead class=" text-primary">
                 <th>#</th>           
-                <th>Hcode</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Status</th>
+                <th>Role id</th>
+                <th>User id</th>
+                <th>Apps id</th>
+                <th>Apps menu id</th>
                 <th>Created at</th>   
                 <th>Updated at</th>   
                 <th>Aksi</th>
               </thead>
               <tbody>            
-                <tr>       
+                <tr>    
                   <td>1</td>
-                  <td>dashboard</td>
-                  <td>Dashboard</td>
-                  <td>Lorem ipsum dolor siamet</td>
+                  <td>6</td>
+                  <td>1</td>
+                  <td>1</td>
                   <td>1</td>
                   <td>2022-08-20 10:46:58</td>
                   <td>2022-08-20 10:46:58</td>
@@ -228,7 +240,7 @@ $aksi   = "module/".$_GET['module']."/action.php";
     <div class="col-md-12">
       <div class="card">
         <div class="card-header card-header-primary">
-          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create"><i class="fa fa-plus"></i></a></span>
+          <span class="float-right"><a class="btn btn-secondary" href="?module=<?php echo $_GET['module'] ?>&act=create_apps_permission"><i class="fa fa-plus"></i></a></span>
           <h4 class="card-title "><?php echo strtolower($_GET['module']) ?> menu permission</h4>
           <p class="card-category"> Tabel dengan data <?php echo strtolower($_GET['module']) ?> menu permission</p>
         </div>
@@ -246,10 +258,8 @@ $aksi   = "module/".$_GET['module']."/action.php";
             <table class="table">
               <thead class=" text-primary">
                 <th>#</th>           
-                <th>Hcode</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Status</th>
+                <th>Role id</th>
+                <th>Permission id</th> 
                 <th>Created at</th>   
                 <th>Updated at</th>   
                 <th>Aksi</th>
@@ -257,9 +267,7 @@ $aksi   = "module/".$_GET['module']."/action.php";
               <tbody>            
                 <tr>       
                   <td>1</td>
-                  <td>dashboard</td>
-                  <td>Dashboard</td>
-                  <td>Lorem ipsum dolor siamet</td>
+                  <td>6</td>
                   <td>1</td>
                   <td>2022-08-20 10:46:58</td>
                   <td>2022-08-20 10:46:58</td>
